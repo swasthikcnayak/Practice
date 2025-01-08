89d8ec1d774cf9619d83fae2da39969dd2f53740
swasthik <swasthik0304@gmail.com>
2025-01-08 23:23:00 +0530

    fix naming

--- lld/questions/parking-lot/ParkingLot.java
+++ lld/questions/parking-lot/ParkingLot.java
@@ -1,11 +1,11 @@
 import java.util.List;
 
-public class Building {
+public class ParkingLot {
     List<Floor> floors;
     List<EntryGate> entryGates;
     List<ExitGate> exitGates;
 
-    Building(List<Floor> floors, List<EntryGate> entryGates, List<ExitGate> exitGates) {
+    ParkingLot(List<Floor> floors, List<EntryGate> entryGates, List<ExitGate> exitGates) {
         this.floors = floors;
         this.entryGates = entryGates;
         this.exitGates = exitGates;
